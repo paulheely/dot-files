d2fc395499df4151250d3acb1d994efae0d5c34c
Paul Heely <pheely@amazon.com>
2020-07-22 07:03:39 -0400

    Changed setup to accomodate an existing .config directory.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+# create config dir if not already there
+mkdir -p ~/.config
+
 # Run stow to setup dot-files and scripts
 stow --dotfiles -v -R -t ~ bash
 stow --dotfiles -v -R -t ~ bin
 stow --dotfiles -v -R -t ~ git
-stow --dotfiles -v -R -t ~ neovim
+stow --dotfiles -v -R -t ~/.config neovim
 stow --dotfiles -v -R -t ~ vim
 stow --dotfiles -v -R -t ~ todo.txt
 stow --dotfiles -v -R -t ~ emacs
