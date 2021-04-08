d116bb29a313df9d6d705cdcf48f82ddfa4852de
Paul Heely <pheely@amazon.com>
2021-04-08 09:03:19 -0400

    Updates and added zsh config.

--- setup.sh
+++ setup.sh
@@ -4,11 +4,12 @@
 mkdir -p ~/.config
 
 # Run stow to setup dot-files and scripts
-stow --dotfiles -v -R -t ~ bash
+#stow --dotfiles -v -R -t ~ bash
 stow --dotfiles -v -R -t ~ bin
 stow --dotfiles -v -R -t ~ git
 stow --dotfiles -v -R -t ~/.config neovim
 stow --dotfiles -v -R -t ~ vim
 stow --dotfiles -v -R -t ~ todo.txt
 stow --dotfiles -v -R -t ~ emacs
+stow --dotfiles -v -R -t ~ zsh
 
